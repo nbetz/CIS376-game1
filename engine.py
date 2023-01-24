d9d705d353d47debde5378aec726c92c618ef1e8
Lren1300 <reneaul@mail.gvsu.edu>
2023-01-24 18:06:12 -0500

    Basic game loop that displays a screen with a grid

--- engine.py
+++ engine.py
@@ -1,1 +1,25 @@
 import pygame
+#Logan Reneau, initial gameloop and display
+
+fps = 10
+pygame.init()
+tile = 30
+width = 720
+height = 480
+display = pygame.display.set_mode(width, height)
+clock = pygame.time.Clock()
+
+while True:
+    display.fill(pygame.Color('black'))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    #draws the lines that make the grids
+    [pygame.draw.line(display,pygame.Color('gray'), (x, 0), (x, height))
+     for x in range (0, width, tile)]
+    [pygame.draw.line(display, pygame.Color('gray'), (0, y), (width, y))
+     for y in range(0, height, tile)]
+
+    pygame.display.flip()
+    clock.tick(fps)
